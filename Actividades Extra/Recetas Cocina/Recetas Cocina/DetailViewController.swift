01f6800373563e2ac94ae561e99fbd1c90c3c4a8
Sergio Arranz Sobrino <sergio.arranz@live.u-tad.com>
2017-06-12 15:27:44 +0200

    Info completa de cada receta + header section

--- Actividades Extra/Recetas Cocina/Recetas Cocina/DetailViewController.swift
+++ Actividades Extra/Recetas Cocina/Recetas Cocina/DetailViewController.swift
@@ -13,8 +13,6 @@ class DetailViewController: UIViewController {
     
     @IBOutlet var recipeName: UILabel!
     @IBOutlet var recipeImageView: UIImageView!
-    @IBOutlet var recipeTime: UILabel!
-    @IBOutlet var recipeIngredients: UILabel!
     
     var recipe : Recipe!
     
@@ -23,8 +21,88 @@ class DetailViewController: UIViewController {
         
         self.recipeName.text = self.recipe.name
         self.recipeImageView.image = self.recipe.image
-        self.recipeTime.text = "\(self.recipe.time!) min."
-        self.recipeIngredients.text = "Nº Ingredientes: \(self.recipe.ingredients.count)"
     }
 
+}
+
+
+extension DetailViewController : UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 3
+        case 1:
+            return self.recipe.ingredients.count
+        case 2:
+            return self.recipe.steps.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailRecipeCell", for: indexPath) as! RecipeDetailViewCell
+        
+        switch indexPath.section {
+        case 0:
+            switch indexPath.row {
+            case 0:
+                cell.keyLabel.text = "Nombre: "
+                cell.valueLabel.text = self.recipe.name
+            case 1:
+                cell.keyLabel.text = "Tiempo: "
+                cell.valueLabel.text = "\(self.recipe.time!) min"
+            case 2:
+                cell.keyLabel.text = "Favorita: "
+                if self.recipe.isFavourite {
+                    cell.valueLabel.text = "Si"
+                } else {
+                    cell.valueLabel.text = "No"
+                }
+            default:
+                break
+            }
+            
+        case 1:
+            if indexPath.row == 0 {
+                cell.keyLabel.text = "Ingredientes:"
+            } else {
+                cell.keyLabel.text = ""
+            }
+            cell.valueLabel.text = self.recipe.ingredients[indexPath.row]
+        case 2:
+            if indexPath.row == 0 {
+                cell.keyLabel.text = "Pasos:"
+            } else {
+                cell.keyLabel.text = ""
+            }
+            cell.valueLabel.text = self.recipe.steps[indexPath.row]
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title = ""
+        
+        switch section {
+        case 1:
+            title = "Ingredientes"
+        case 2:
+            title = "Pasos"
+        default:
+            break
+        }
+        return title
+    }
+}
+
+extension DetailViewController : UITableViewDelegate {
+    
 }
---
+++ Actividades Extra/Recetas Cocina/Recetas Cocina/RecipeDetailViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeDetailViewCell.swift
+//  Recetas Cocina
+//
+//  Created by Sergio Arranz on 12/6/17.
+//  Copyright © 2017 Sergio Arranz Sobrino. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewCell: UITableViewCell {
+    
+    @IBOutlet var keyLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
