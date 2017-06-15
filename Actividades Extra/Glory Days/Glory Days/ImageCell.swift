32c7e10d8cf3ff9e5d27d021c82784710c077db3
Sergio Arranz Sobrino <sergio.arranz@live.u-tad.com>
2017-06-15 18:24:44 +0200

    Inicio Proyecto: diseño y vinculación
    Diseño general de la app + vinculación de elementos visuales a código

---
+++ Actividades Extra/Glory Days/Glory Days/ImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCell.swift
+//  Glory Days
+//
+//  Created by Sergio Arranz Sobrino on 15/6/17.
+//  Copyright © 2017 Sergio Arranz Sobrino. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    
+}
---
+++ Actividades Extra/Glory Days/Glory Days/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Glory Days
+//
+//  Created by Sergio Arranz Sobrino on 15/6/17.
+//  Copyright © 2017 Sergio Arranz Sobrino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var InfoLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func askForPermissions(_ sender: UIButton) {
+        
+    }
+
+}
+
