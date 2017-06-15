985fa11c0778fbf9f5bb54e129ee4d53456f082c
Sergio Arranz Sobrino <sergio.arranz@live.u-tad.com>
2017-06-15 18:54:58 +0200

    Funciones para pedir permisos con Threading
    Funciones para pedir permisos con hilos, uno encadenado tras de otro.

--- Actividades Extra/Glory Days/Glory Days/ViewController.swift
+++ Actividades Extra/Glory Days/Glory Days/ViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+import AVFoundation
+import Photos
+import Speech
+
+
 class ViewController: UIViewController {
 
     @IBOutlet var InfoLabel: UILabel!
@@ -24,6 +29,24 @@ class ViewController: UIViewController {
     @IBAction func askForPermissions(_ sender: UIButton) {
         
     }
+    
+    func askForPhotosPermissions() {
+        PHPhotoLibrary.requestAuthorization { [unowned self] (authStatus) in
+            DispatchQueue.main.async { // Manda al hilo principal todo el código que contiene para que pedir permisos o la etiqueta del else deje de ocurrir en hilos secundarios
+            if authStatus == .authorized {
+                self.askForRecordPermissions()
+            } else {
+                self.InfoLabel.text = "Has denegado el permiso de fotos. Por favor, activalo en los ajustes del dispositivo para continuar."
+            }
+            }
+        }
+    }
 
+    func askForRecordPermissions() {
+        AVAudioSession.sharedInstance().requestRecordPermission { [unowned self] (allowed) in
+            <#code#>
+        }
+        
+    }
 }
 
